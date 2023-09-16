29ba84505b85550a3fcd113611ffe935fcf7eca9
triple93r <56282248+triple93r@users.noreply.github.com>
2023-09-16 08:44:43 +0530

    init3

---
+++ SingletonPattern/SingletonTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace SingletonPattern
+{
+    public class SingletonTest
+    {
+        private static SingletonTest _instance;
+        private static readonly object _instanceLock = new object();
+        private SingletonTest()
+        {
+
+        }
+        public static SingletonTest Instance()
+        {
+            if( _instance == null )
+            {
+                lock ( _instanceLock )
+                {
+                    if( _instance == null )
+                    {
+                        _instance = new SingletonTest();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
